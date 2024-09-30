e372800a7a808ad6d613e4403d143df63b3667ca
aravind2347214 <aravind.nandakumar@mca.christuniversity.in>
2024-09-30 11:21:45 +0530

    push

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app= express()
-const PORT=8000;
+const PORT=8001;
 var cors = require('cors');
 const { connectToDatabase } = require("./database");
 const authRouter = require("./routes/auth");
